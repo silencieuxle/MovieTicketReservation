e4cf95dd34df7e6e9e6f1d0470c519c5c7c5eccc
silencieux.le <silencieux.le@gmail.com>
2015-04-30 10:54:24 +0700

    Restructure whole project (1/2)

---
+++ MovieTicketReservation/Services/IMemberRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MovieTicketReservation.Models;
+
+namespace MovieTicketReservation.Services {
+    public interface IMemberRepository<Member>  {
+        IEnumerable<Member> GetAll();
+        Member GetByID(int id);
+        Member Add(Member member);
+    }
+}
